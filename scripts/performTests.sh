12dd43b84f0e61b2c6cc3867a7a39b1650def777
Stephan Esch <stephan@esch.cc>
2017-01-27 00:23:59 +0100

    need output

--- scripts/performTests.sh
+++ scripts/performTests.sh
@@ -41,7 +41,8 @@ fi
 cp scripts/$HOMEBRIDGE_CONFIG "$TEST_CONFIG_DIR/config.json"
 
 # Start homebridge
-$HOMEBRIDGE_BINARY -U $TEST_CONFIG_DIR -P $HOMEBRIDGE_SERVER_DIR >/dev/null 2>&1 &
+# $HOMEBRIDGE_BINARY -U $TEST_CONFIG_DIR -P $HOMEBRIDGE_SERVER_DIR >/dev/null 2>&1 &
+$HOMEBRIDGE_BINARY -U $TEST_CONFIG_DIR -P $HOMEBRIDGE_SERVER_DIR &
 
 # Give homebridge 5 seconds to be ready
 echo "Waiting 5 seconds for homebridge to start..."
